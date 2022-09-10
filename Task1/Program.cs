0a8d8b5081a5db775048bad104e3b2e46ad0edc7
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-10 21:43:14 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// ПОКАЗАТЬ ДВУМЕРНЫЙ МАССИВ РАЗМЕРОМ MxN, ЗАПОЛНЕННЫЙ ВЕЩЕСТВЕННЫМИ ЧИСЛАМИ
+
+
+void FillArray(double[,] matr)
+{
+    for(int m=0; m < matr.GetLength(0); m++)
+    { for (int n=0; n < matr.GetLength(1); n++)
+        { 
+            matr[m,n] = new Random().NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] matr)
+{
+    for(int m=0; m < matr.GetLength(0); m++)
+    { for (int n=0; n < matr.GetLength(1); n++)
+        { 
+            Console.Write($"{matr[m,n]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = new double[5, 7];
+FillArray(matrix);
+PrintArray(matrix);
