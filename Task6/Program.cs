a83edcce62c5e96f2d78e49265058f4b8ac9636c
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-10 21:47:41 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,59 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива
+
+Console.Clear();
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] SortNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+
+    }
+    return array;
+}
+
+void PrintTable(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 7;
+int col = 15;
+int min = 10;
+int max = 1000;
+
+int[,] table = GetMatrix(row, col, min, max);
+PrintTable(table);
+
+Console.WriteLine();
+int[,] rowSort = SortNumbers(table);
+PrintTable(rowSort);
